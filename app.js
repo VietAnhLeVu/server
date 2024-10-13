5aa6171b5a7a197480b7d7942a7b030b84d8bece
Le Vu Viet Anh <vietanhlevu2004@gmail.com>
2024-10-13 21:37:05 +0700

    test

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -16,24 +17,27 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 // Kết nối tới MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log('MongoDB connected'))
+//   .catch(err => console.error(err));
 
-// API để lấy danh sách người dùng
-app.get('/api/users', async (req, res) => {
-  const users = await User.find();
-  res.json(users);
-});
+// // API để lấy danh sách người dùng
+// app.get('/api/users', async (req, res) => {
+//   const users = await User.find();
+//   res.json(users);
+// });
 
 // API để thêm người dùng
-app.post('/api/users', async (req, res) => {
-  const newUser = new User({ name: req.body.name });
-  await newUser.save();
-  res.json(newUser);
-});
+// app.post('/api/users', async (req, res) => {
+//   const newUser = new User({ name: req.body.name });
+//   await newUser.save();
+//   res.json(newUser);
+// });
 
 const PORT = process.env.PORT || 4000;
+app.get('/api/health', async (req,res) => {
+  return res.json("good")
+})
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
